3faf058b2b9a1051bca50d46fc4a9a22ddee61e6
lchaconc <luis.chacon.campos@gmail.com>
2018-12-02 00:46:08 -0600

    imp 2
    despliegue de titulo y descripción
    se copia libreria jq ui

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+"use strict";
+var spriteSelected, nivel = 1 ;
+
 $(document).ready(function () {
     loadEvents();
 });
@@ -5,7 +8,11 @@ $(document).ready(function () {
 function loadEvents() {
     console.log("events");
 
-    $(".sprites").mouseenter(function () { 
+    $(".sprites").mouseenter(function () {
+        spriteSelected = this.id;
+        //console.log(spriteSelected); 
+        showInfo();
+                
         $( this ).effect( "bounce", "slow" );
     });
 
@@ -14,16 +21,50 @@ function loadEvents() {
     });
 
 
+    $(".sprites").mouseleave(function () { 
+        $(".div-info").slideUp();
+    });
+
+
   }
 
+function showInfo() {   
+    switch (spriteSelected) {
+        case "imgSesion1":           
+            $(".span-titulo").text("Unidad 1: Iniciando la preparación"); 
+            $(".parrafo-Info").text("Preparación y contectualización para la creación de experimentos. Se obtienen los instrumentos para el laboratorio. ");          
+        break;
+        case "imgSesion2":
+            $(".span-titulo").text("Unidad 2: Reforzando el conocimiento"); 
+            $(".parrafo-Info").text("Se profundiza en elementos para la creación de juegos y actividades interactivas ");          
+        break;
+        case "imgSesion3":
+            $(".span-titulo").text("Unidad 3: Nuestro primer experimento"); 
+            $(".parrafo-Info").text("En este momento tenemos la posibilidad de crear nuestro primer experimento sin que haya peligro... ");          
+        break;
+    
+        default:
+        console.log("opcion fuera de rango");
+            break;
+    }
+    $(".div-info").slideDown();
+}
 
-  function routing (opt) {
-      //console.log(opt);
-      window.location="./pages/engranaje.html"
-    /*  
-      switch (opt) {
+
+
+  function routing () {    
+      switch (spriteSelected) {
           case "imgSesion1":
             window.location="./pages/engranaje.html"
+            break;
+            case "imgSesion2":
+            $( "#"+spriteSelected ).fadeIn( );
+      
+            break;
+            case "imgSesion3":
+            console.log("3");
+            $( "#"+spriteSelected ).fadeIn( );
+        
             break;
       
           default:
@@ -31,6 +72,6 @@ function loadEvents() {
           
               break;
       }
-      */
+      
       
   }
